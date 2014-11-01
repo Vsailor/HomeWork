4cba3af67495e30cdea3e74c90ff547b66fa97a4
Ivan <sirius9764@gmail.com>
2014-11-01 21:31:38 +0200

    Done output function

--- RoundList/RoundList/main.cpp
+++ RoundList/RoundList/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include "rlist.h"
 using namespace std;
-
+void output(rlist a) {
+	int save = cur(a);
+	do {
+		cout << cur(a);
+		toNext(a);
+	} while (save != cur(a));
+}
 void main() {
 	rlist a;
 	Init(a);
@@ -9,11 +15,8 @@ void main() {
 	Insert(a, 2);
 	Insert(a, 3);
 	Insert(a, 4);
-	while (Length(a) != 0) {
-		cout << cur(a);
-		toNext(a);
-	}
-	cout << Length(a);
+	output(a);
+	cout << endl << Length(a) << endl;
 
 	system("pause");
 }
